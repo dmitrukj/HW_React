9355c3e158229853f5e17b6d07eb19748a4f8d70
dmitruk <dmitruk_89@mail.ru>
2022-02-23 13:51:29 +0300

    finish HW

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const items = [
+  { id: "uniq1", name: "Paul" },
+  { id: "uniq2", name: "Mary" },
+  { id: "uniq3", name: "Viktor" },
+  { id: "uniq4", name: "Suzanna" },
+  { id: "uniq5", name: "Peter" },
+  { id: "uniq6", name: "Vladislav" },
+  { id: "uniq7", name: "Anna" },
+];
+
+const User = (props) => {
+  let userName = props.name;
+  let newName = userName.indexOf("e");
+  let name = props.name;
+  if (props.name.length > 5) {
+    name = props.name.slice(0, 5) + "...";
+  }
+  let itemsName = name.toLowerCase();
+  let newItemsName = itemsName.split("").reverse().join("");
+  let itemsNameRed = "";
+  if (itemsName === newItemsName) {
+    itemsNameRed = "name-highlighted";
+  }
+  if (newName === -1) {
+    console.log(props.name.length);
+
+    return (
+      <div>
+        <span className={itemsNameRed}> it`s user {name} </span>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <span className={itemsNameRed}>
+          It`s Premium User! Welcome, {name}!
+        </span>
+      </div>
+    );
+  }
+};
+
+// export const UserList = () => {
+//   return (
+//     <div>
+//       {items.map((item) => (
+//         <span key={item.id}>
+//           <User name={item.name} />
+//         </span>
+//       ))}
+//     </div>
+//   );
+// };
+
+export const UserList = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ComposableUser>
+      {items.map((item) => (
+        <span key={item.id}>
+          <User name={item.name} />
+        </span>
+      ))}
+    </ComposableUser>
   );
-}
+};
 
-export default App;
+const ComposableUser = (props) => {
+  return <div>{props.children}</div>;
+};
+export default UserList;
